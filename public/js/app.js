0d7dfe7d32f30d6994d5f876278e48867cc140fd
courtney <coa.mcallister@gmail.com>
2017-02-27 20:55:02 -0500

    successfully implemented a Vue application that displays the current weather data from the Vue instance.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log('working');
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'vue is still working'
+  }
+});
+
+var currentlyWidget = new Vue({
+  el: '#currently',
+  data: {
+    time: 10000000,
+    summary: 'Partly Cloudy',
+    icon: 'partly-cloudy',
+    apparentTemperature: 75.1,
+    precipProbability: 0.30,
+    humidity: 0.61,
+    location: 'Gainesville, FL'
+  }
+});
