536c0bf7519cd7a93ea964175f72e833434aaf14
courtney <coa.mcallister@gmail.com>
2017-03-01 19:43:44 -0500

    added dynamic summary and icon for daily widget. added icons for every type of weather.

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ var currentlyWidget = new Vue({
   data: {
     time: 10000000,
     summary: 'Partly Cloudy',
-    icon: 'partly-cloudy',
+    icon: 'partly-cloudy-day',
     apparentTemperature: 75.1,
     precipProbability: 0.30,
     humidity: 0.61,
@@ -31,6 +31,40 @@ var currentlyWidget = new Vue({
            currentlyWidget.apparentTemperature = data.apparentTemperature;
            currentlyWidget.precipProbability = data.precipProbability;
            currentlyWidget.humidity = data.humidity;
+         })
+         .catch(function(error){
+           console.log(error);
+         });
+  }
+});
+
+var dailyWidget = new Vue({
+  el: '#daily',
+  data: {
+    summary: 'not dynamic',
+    icon: 'partly-cloudy-day',
+    location: 'Gainesville, FL'
+  },
+  methods: {
+    iconUrl: function(iconString){
+      return `images/${iconString}.png`;
+    }
+  },
+  created: function(){
+    axios.get('/weather/29.1, -81.4')
+         .then(function(response){
+           var data = response.data.daily;
+           dailyWidget.summary = data.summary;
+           dailyWidget.icon = data.icon;
+
+            // Object.keys(dailyWidget._data)
+            //       .forEach(function(property){
+            //         var data = response.data.daily;
+            //         dataKey = eval('dailyWidget.' + property);
+            //         dataKey = eval('data.' + property);
+            //         console.log(dataKey);
+            //       })
+
          })
          .catch(function(error){
            console.log(error);
