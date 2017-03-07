cfed106e7bd70f7f440917b4e5b0715038067fa5
courtney <coa.mcallister@gmail.com>
2017-03-07 18:52:57 -0500

    Replaced darksky API key with a new key. Added CSS styling for each widget, and connected the location of each widget so that it updates in each place when the search button is clicked.

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,17 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'vue is still working'
-  }
-});
-
 var currentlyWidget = new Vue({
   el: '#currently',
   data: {
     time: 10000000,
     summary: 'Partly Cloudy',
     icon: 'partly-cloudy-day',
+    temperature: 70,
     apparentTemperature: 75.1,
     precipProbability: 0.30,
     humidity: 0.61,
     location: 'Gainesville, FL',
     latitude: 29.1,
-    longitude: -81.4
+    longitude: -81.4,
+    windSpeed: 10
   },
   methods: {
     iconUrl: function(iconString){
@@ -24,7 +19,7 @@ var currentlyWidget = new Vue({
     },
     getWeather: function(lat, lon){
       var url = `/weather/${lat},${lon}`;
-      axios.get(url) //gets data and updates the state
+      axios.get(url)
            .then(function(response){
              var data = response.data.currently;
              Object.keys(data).forEach(function(dataLabel){
@@ -35,13 +30,45 @@ var currentlyWidget = new Vue({
              console.log(error);
            });
     },
-    updateWeather: function(){
-      this.getWeather(this.latitude,this.longitude);
-    },
+    updateLocation: function(address){
+      var url = `/location/${address}`;
+      axios.get(url)
+           .then(function(response){
+             var loc = response.data.results[0].geometry.location;
+             var newAddress = response.data.results[0].formatted_address;
+             this.latitude = loc.lat;
+             this.longitude = loc.lng;
+             this.getWeather(this.latitude, this.longitude);
+             this.location = newAddress;
+             minutelyWidget.location = newAddress;
+             hourlyWidget.location = newAddress;
+             dailyWidget.location = newAddress;
+             minutelyWidget.getMinutelyWeather(this.latitude, this.longitude);
+             hourlyWidget.getHourlyWeather(this.latitude, this.longitude);
+             dailyWidget.getDailyWeather(this.latitude, this.longitude);
+           }.bind(this))
+           .catch(function(error){
+             console.log(error);
+           });
 
+    },
+    getDate: function(seconds){
+      var date = new Date(seconds * 1000);
+      var weekday = date.getDay();
+      var day = date.getDate();
+      var month = date.getMonth();
+      var dayOfTheWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var monthOfTheYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      return `${dayOfTheWeek[weekday]}, ${monthOfTheYear[month]} ${day} at ${hour % 12}:${minutes < 10 ? '0' + minutes : minutes}`;
+    },
+    formatPercents: function(percent){
+      return percent * 100;
+    },
   },
   created: function(){
-    this.getWeather(29.1, -81.4);
+    this.getWeather(this.latitude, this.longitude);
   }
 });
 
@@ -50,8 +77,8 @@ var hourlyWidget = new Vue({
   data: {
     summary: "it's gonna rain!",
     icon: "rain",
+    location: "Gainesville, FL",
     hours: []
-
   },
   methods: {
     getMainIcon: function(){
@@ -93,32 +120,88 @@ var dailyWidget = new Vue({
   data: {
     summary: 'Partly Cloudy',
     icon: 'partly-cloudy-day',
-    location: 'Gainesville, FL'
+    time: 100000,
+    location: 'Gainesville, FL',
+    days: []
   },
   methods: {
-    iconUrl: function(iconString){
-      return `images/${iconString}.png`;
+    getDailyIcon: function(iconString){
+      return `/images/${iconString}.png`;
     },
-    getWeather: function(lat, lon){
+    getDailyWeather: function(lat, lon){
       var url = `/weather/${lat},${lon}`;
       axios.get(url) //gets data and updates the state
            .then(function(response){
-             var data = response.data.daily;
-             Object.keys(data).forEach(function(dataLabel){
-               dailyWidget[dataLabel] = data[dataLabel];
+             var dailyData = response.data.daily;
+             Object.keys(dailyData).forEach(function(dataLabel){
+               dailyWidget[dataLabel] = dailyData[dataLabel];
              })
-           })
+             this.days = dailyData.data;
+           }.bind(this))
            .catch(function(error){
              console.log(error);
            });
     },
     updateWeather: function(){
-      this.getWeather(this.latitude,this.longitude);
+      this.getDailyWeather(this.latitude,this.longitude);
+
+    },
+    getDate: function(seconds){
+      var date = new Date(seconds * 1000);
+      var weekday = date.getDay();
+      var day = date.getDate();
+      var month = date.getMonth();
+      var dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var monthOfTheYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      return `${dayOfTheWeek[weekday]}, ${monthOfTheYear[month]} ${day}`;
     }
-
-
   },
   created: function(){
-    this.getWeather(29.1, -81.4);
+    this.getDailyWeather(29.1, -81.4);
+    this.getDate(this.time);
+  }
+});
+
+var minutelyWidget = new Vue({
+  el: '#minutely',
+  data: {
+    summary: 'Partly Cloudy',
+    icon: 'partly-cloudy-day',
+    location: 'Gainesville, FL',
+    precipProbability: 5,
+    precipIntensity: 5,
+    minutes: []
+  },
+  methods: {
+    getIcon: function(){
+      return `/images/${this.icon}.png`;
+      console.log(iconString);
+    },
+    getMinutelyWeather: function(lat, lon){
+      var url = `/weather/${lat},${lon}`;
+      axios.get(url)
+           .then(function(response){
+             var minutelyData = response.data.minutely;
+             Object.keys(minutelyData).forEach(function(property){
+               minutelyWidget[property] = minutelyData[property];
+             })
+             this.minutes = minutelyData.data;
+           }.bind(this))
+           .catch(function(error){
+             console.log(error);
+           });
+    },
+    updateWeather: function(){
+      this.getMinutelyWeather(this.latitude, this.longitude)
+    },
+    getDate: function(seconds){
+      var date = new Date(seconds * 1000);
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      return `${hour % 12}:${minutes < 10 ? '0' + minutes : minutes}`;
+    }
+  },
+  created: function(){
+    this.getMinutelyWeather(29.1,-81.4);
   }
 });
